ef452efe304d3fb199ada37af7ac92867496da5e
buzzcodeaarefa-source <buzzcodeaarefa@gmail.com>
2026-05-20 20:55:23 +0500

    datatypes and operators

---
+++ datatype.java
@@ -0,0 +1,14 @@
+public class datatype {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 15;
+        int sum = a + b;
+        int diff = a - b;
+        int mult = a * b;
+        int div = a/b;
+        System.out.println("addition of a and b is:" + sum);
+        System.out.println("subtraction of a and b is:" + diff);
+        System.out.println("multiplication of a and b is:"+ mult);
+        System.out.println("division of a and b is:" + div);
+    }
+}
---
+++ method.java
@@ -0,0 +1,15 @@
+class method {
+
+    public static void main(String[] args) {
+        System.out.println("My Coffee Recipe");
+        Coffee();
+    }
+
+    public static void Coffee() {
+        System.out.println("1. Boil water in pan");
+        System.out.println("2. Add coffee powder");
+        System.out.println("3. Add sugar");
+        System.out.println("4. Pour milk and stir well");
+        System.out.println("5. Serve hot coffee");
+    }
+}
